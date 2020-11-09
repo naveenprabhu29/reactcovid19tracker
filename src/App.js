6df4dae30b76a16a4e42a8467fd7b25df6e94ccf
Naveen Prabhu <naveenprabhuje@gmail.com>
2020-11-09 16:06:50 +0530

    only total cases

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect , useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 
 function App() {
+  const [latest,setLatest]=useState(""); 
+  useEffect(()=>{
+    axios.get('https://coronavirus-19-api.herokuapp.com/all ').then(res=>{
+      setLatest(res.data);
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <CardDeck>
+  <Card className='text-center' bg='warning' text='white'
+  border="dark" style={{margin:'10px'}}  >
+    <Card.Body>
+      <Card.Title>Cases</Card.Title>
+      <Card.Text>
+        {latest.cases}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small>Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card  className='text-center'
+  bg='danger' text='white'
+  border="dark" 
+  style={{margin:'10px'}} >
+    <Card.Body>
+      <Card.Title>Cases</Card.Title>
+      <Card.Text>
+      {latest.deaths}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small>Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card className='text-center'
+  bg='success' text='white'
+  border="dark"  style={{margin:'10px'}} >
+    <Card.Body>
+      <Card.Title>Cases</Card.Title>
+      <Card.Text>
+      {latest.recovered}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small>Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  
+</CardDeck>
     </div>
   );
 }
